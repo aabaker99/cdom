4bc4d7a81abc3ecde3a755bfb647758571195488
Aaron <aabaker99@gmail.com>
2019-07-15 15:33:09 -0700

    protect against accidental double runs

--- create_protein_to_ipr.sql
+++ create_protein_to_ipr.sql
@@ -1,5 +1,6 @@
-create table if not exists protein2ipr (uniprotswissprot, interpro, interproname, sourceid, domainstart INTEGER, domainstop INTEGER);
-create index if not exists protein2ipr_uniprot on protein2ipr (uniprotswissprot);
-create index if not exists protein2ipr_interpro on protein2ipr (interpro);
+.bail on
+create table protein2ipr (uniprotswissprot, interpro, interproname, sourceid, domainstart INTEGER, domainstop INTEGER);
+create index protein2ipr_uniprot on protein2ipr (uniprotswissprot);
+create index protein2ipr_interpro on protein2ipr (interpro);
 .separator "\t"
 .import protein2ipr.dat protein2ipr
