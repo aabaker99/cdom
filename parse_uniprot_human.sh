4b9afad2874135782dae3c37adad68db1c23e13e
Aaron <aabaker99@gmail.com>
2019-07-15 13:53:04 -0700

    remove old parser

--- parse_uniprot_human.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-cat uniprot_sprot_human.dat | grep -E '^AC' | sed 's/^AC\W\W*//' | awk -F';' '{ for (i=1;i<NF;i++) { print $i }}' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' | sort | uniq > uniprot_sprot_human.txt
